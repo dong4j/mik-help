72fa0eea2e6c078f1eba3d8aa40b5fb0e88d157e
dong4j <dong4j@gmail.com>
2019-03-25 17:49:50 +0800

    :bento: 添加 License

--- src/main/java/info/dong4j/idea/plugin/help/HelpApplication.java
+++ src/main/java/info/dong4j/idea/plugin/help/HelpApplication.java
@@ -25,10 +25,8 @@
 
 package info.dong4j.idea.plugin.help;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
