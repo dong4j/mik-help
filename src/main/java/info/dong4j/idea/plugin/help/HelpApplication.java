189b453901a75e7c1d3bb1220389bb5a226e6eb8
dong4j <dong4j@gmail.com>
2019-03-25 17:37:04 +0800

    init

---
+++ src/main/java/info/dong4j/idea/plugin/help/HelpApplication.java
@@ -0,0 +1,77 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 dong4j
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package info.dong4j.idea.plugin.help;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * <p>Company: 科大讯飞股份有限公司-四川分公司</p>
+ * <p>Description: </p>
+ *
+ * @author dong4j
+ * @email sjdong3@iflytek.com
+ * @since 2019-03-25 14:35
+ */
+@RestController
+@SpringBootApplication
+public class HelpApplication {
+    private static Properties properties = new Properties();
+
+    @RequestMapping("/mik/help/{where}")
+    public Map<String, String> helpUrl(@PathVariable("where") String where) {
+        Map<String, String> result = new HashMap<>(2);
+        // todo-dong4j : (2019年03月25日 14:39) [处理 where]
+        reload();
+        result.put("code", "200");
+        result.put("url", properties.getProperty("mik.url"));
+        return result;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpApplication.class);
+    }
+
+    private static void reload() {
+
+        try {
+            String configPath = System.getProperty("config.path");
+            properties.load(new FileReader(configPath + "application.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
