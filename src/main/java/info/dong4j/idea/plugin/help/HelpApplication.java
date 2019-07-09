6fd02d023ef5e8ffbbe5906bab68940dc6c4f2e3
dong4j <code.dsj@gmail.com>
2019-07-09 16:22:34 +0800

    :wrench: 修改公司和邮箱

--- src/main/java/info/dong4j/idea/plugin/help/HelpApplication.java
+++ src/main/java/info/dong4j/idea/plugin/help/HelpApplication.java
@@ -37,11 +37,11 @@ import java.util.Map;
 import java.util.Properties;
 
 /**
- * <p>Company: 科大讯飞股份有限公司-四川分公司</p>
+ * <p>Company: no company</p>
  * <p>Description: </p>
  *
  * @author dong4j
- * @email sjdong3@iflytek.com
+ * @email dong4j@gmail.com
  * @since 2019-03-25 14:35
  */
 @RestController
